17ffe3ee93a8aae93beedb3a9afb98d47f6db6ee
Michał Nowotnik <m.nowot@gmail.com>
2017-03-18 18:23:12 +0100

    Options + help in server

--- server/fovrisd.cc
+++ server/fovrisd.cc
@@ -11,7 +11,35 @@ std::string makeErrorMessage(const fovris::ParsingError &e) {
            std::to_string(e.getPos());
 }
 
+std::string makeQueryErrorMessage(const fovris::InvalidInputException &e,
+                                  const std::string &query) {
+    return "Error in query: " + query + " . Reason: " + e.what();
+}
+
+void printHelp() {
+    std::cout << "Usage: fovrisd [ADDR] [PORT]\n";
+    std::cout << "Minimal stateless server that accepts 4QL scripts\n";
+    std::cout << "and responds with results.\n\n";
+    std::cout << "How to use:\n";
+    std::cout << "Send POST request to the /query endpoint with 4QL\n";
+    std::cout << "script in the body.\n";
+}
+
 int main(int argc, char **argv) {
+
+    if (argc != 3 && argc != 1) {
+        printHelp();
+        return 1;
+    }
+
+    std::string addr = "0.0.0.0";
+    int port = 8080;
+
+    if (argc == 3) {
+        addr = std::string(argv[1]);
+        port = std::stoi(argv[2]);
+    }
+
     crow::SimpleApp app;
 
     CROW_ROUTE(app, "/query")
@@ -32,12 +60,13 @@ int main(int argc, char **argv) {
             for (auto &query : sp.getQueries()) {
                 std::ostringstream ss;
                 try {
-                    ss << fovris::QueryResultPrinter::Ql(program.query(query));
+                    std::string resultStr = fovris::print(
+                        fovris::QueryResultPrinter::Ql(program.query(query)));
+                    resp.write(resultStr);
                 } catch (fovris::InvalidInputException &e) {
-                    return crow::response(400, e.what());
+                    return crow::response(
+                        400, makeQueryErrorMessage(e, fovris::print(query)));
                 }
-                std::string resultStr = ss.str();
-                resp.write(resultStr);
                 resp.write("\n");
             }
 
@@ -47,5 +76,7 @@ int main(int argc, char **argv) {
 
             return resp;
         });
-    app.port(8080).multithreaded().run();
+
+    CROW_LOG_INFO << "Starting server: " << addr << ":" << port;
+    app.bindaddr(addr).port(port).multithreaded().run();
 }
