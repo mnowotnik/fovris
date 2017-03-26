c4e9eeff6e7d4c00830bc25c606b0f254391ea8b
Michał Nowotnik <m.nowot@gmail.com>
2017-03-26 03:53:49 +0200

    supporting CORS requests (partially)

--- server/fovrisd.cc
+++ server/fovrisd.cc
@@ -1,10 +1,14 @@
-#include "crow.h"
 #include "fovris/Program.h"
 #include "fovris/QlDeserializer.h"
 #include "fovris/QueryResultPrinter.h"
 
+#include "crow.h"
+#include <boost/algorithm/string.hpp>
+
+#include <algorithm>
 #include <ostream>
 #include <string>
+#include <vector>
 
 std::string makeErrorMessage(const fovris::ParsingError &e) {
     return "Error at line  " + std::to_string(e.getLine()) + " ,position " +
@@ -25,6 +29,72 @@ void printHelp() {
     std::cout << "script in the body.\n";
 }
 
+const std::vector<std::string> accessControlRequestMethods = {"GET", "POST",
+                                                              "PUT", "DELETE"};
+
+crow::response queryOptions(const crow::request &req) {
+    crow::response resp;
+    if (req.get_header_value("Access-Control-Request-Method") == "") {
+        resp.code = 400;
+        resp.body = "Invalid preflight request";
+        return resp;
+    }
+
+    const std::string reqMethod =
+        req.get_header_value("Access-Control-Request-Method");
+
+    if (!std::any_of(std::begin(accessControlRequestMethods),
+                     std::end(accessControlRequestMethods),
+                     std::bind(std::equal_to<std::string>(), reqMethod,
+                               std::placeholders::_1))) {
+        resp.code = 400;
+        resp.body = "Invalid preflight request";
+        return resp;
+    }
+
+    resp.set_header("Access-Control-Allow-Methods", "GET, POST");
+    resp.set_header("Access-Control-Allow-Origin", "*");
+    resp.set_header("Access-Control-Allow-Headers", "Access-Control-Allow-Methods, Access-Control-Allow-Origin");
+    return resp;
+}
+
+crow::response queryPost(const crow::request &req) {
+    fovris::QlDeserializer sp;
+    crow::response resp;
+    resp.set_header("Access-Control-Allow-Origin", "*");
+    try {
+        sp.parse(req.body);
+    } catch (fovris::ParsingError &e) {
+        resp.body = makeErrorMessage(e);
+        resp.code = 400;
+        return resp;
+    }
+    fovris::Program program(fovris::Algorithm::Seminaive);
+    for (auto &module : sp.getModules()) {
+        program.addModule(module);
+    }
+    program.evaluate();
+
+    for (auto &query : sp.getQueries()) {
+        std::ostringstream ss;
+        try {
+            std::string resultStr = fovris::print(
+                fovris::QueryResultPrinter::Ql(program.query(query)));
+            resp.write(resultStr);
+        } catch (fovris::InvalidInputException &e) {
+            resp.body = makeQueryErrorMessage(e, fovris::print(query));
+            resp.code = 400;
+            return resp;
+        }
+        resp.write("\n");
+    }
+
+    if (sp.getQueries().empty()) {
+        resp.write("Result is empty.");
+    }
+    return resp;
+}
+
 int main(int argc, char **argv) {
 
     if (argc != 3 && argc != 1) {
@@ -43,41 +113,13 @@ int main(int argc, char **argv) {
     crow::SimpleApp app;
 
     CROW_ROUTE(app, "/query")
-        .methods("POST"_method)([](const crow::request &req) {
-            fovris::QlDeserializer sp;
-            crow::response resp;
-            resp.add_header("Access-Control-Allow-Origin", "*");
-            try {
-                sp.parse(req.body);
-            } catch (fovris::ParsingError &e) {
-                resp.body = makeErrorMessage(e);
-                resp.code = 400;
-                return resp;
+        .methods("OPTIONS"_method, "POST"_method)([](const crow::request &req) {
+            if (req.method == "POST"_method) {
+                return queryPost(req);
+            } else {
+                return queryOptions(req);
             }
-            fovris::Program program(fovris::Algorithm::Seminaive);
-            for (auto &module : sp.getModules()) {
-                program.addModule(module);
-            }
-            program.evaluate();
-
-            for (auto &query : sp.getQueries()) {
-                std::ostringstream ss;
-                try {
-                    std::string resultStr = fovris::print(
-                        fovris::QueryResultPrinter::Ql(program.query(query)));
-                    resp.write(resultStr);
-                } catch (fovris::InvalidInputException &e) {
-                    return crow::response(
-                        400, makeQueryErrorMessage(e, fovris::print(query)));
-                }
-                resp.write("\n");
-            }
-
-            if (sp.getQueries().empty()) {
-                resp.write("Result is empty.");
-            }
-
-            return resp;
+            return crow::response{};
         });
 
     CROW_LOG_INFO << "Starting server: " << addr << ":" << port;
